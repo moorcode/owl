c6499f7de36eb0f08cb3da9b850c9ddee6231287
Christina Poole <codenamedchris@gmail.com>
2020-11-10 22:43:18 -0500

    Add Participants component skeleton

---
+++ src/components/Participants/Psrticipants.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Participants extends Component {
+  render() {
+    return (
+      <div className="Participants">
+      </div>
+    );
+  }
+}
+
+export default Participants;
