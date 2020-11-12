1e0cfa4dd62033be3099a52f43fe4ed9291058cb
Christina Poole <codenamedchris@gmail.com>
2020-11-11 22:48:07 -0500

    add passing smoke tests and style to new List and Participant components

---
+++ src/components/Participant/Participant.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import STORE from '../List/store';
+import './Participant.css'
+
+class Participant extends Component {
+  render() {
+    return (
+      <li>
+        <img alt='avatar' src={this.props.avatar} />
+        <p>{this.props.name}</p>
+        
+      </li>)
+  }
+}
+export default Participant;
